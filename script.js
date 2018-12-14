ce40de7ef333fb9a723cb0d4717b1b45d0c5519d
Alex Stachowiak <alexander@computer.org>
2018-12-13 20:43:41 -0600

    Create script.js
    Reload page if change is detected in monitored endpoint

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+  let currentPageTimestamp = null;
+  setInterval(() => {
+    fetch(window.location.href + '.update')
+      .then(response => response.text())
+      .then(timestamp => {
+        if (currentPageTimestamp == null)
+          currentPageTimestamp = timestamp;
+        else if (currentPageTimestamp != timestamp)
+          window.location.href = window.location.href;
+    })
+  }, 1000);
+</script>
