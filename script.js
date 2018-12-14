5d5f2b8620d2e21dbdf6cc9bfdf98668d76ea408
Alex Stachowiak <alexander@computer.org>
2018-12-13 20:46:13 -0600

    Web client monitors current page for updates

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-<script>
-  let currentPageTimestamp = null;
-  setInterval(() => {
-    fetch(window.location.href + '.update')
-      .then(response => response.text())
-      .then(timestamp => {
-        if (currentPageTimestamp == null)
-          currentPageTimestamp = timestamp;
-        else if (currentPageTimestamp != timestamp)
-          window.location.href = window.location.href;
-    })
-  }, 1000);
-</script>
+// Monitor current URL + '.update' for changes
+let currentPageTimestamp = null;
+setInterval(() => {
+  fetch(window.location.href + '.update')
+    .then(response => response.text())
+    .then(timestamp => {
+      if (currentPageTimestamp == null)
+        currentPageTimestamp = timestamp;
+      else if (currentPageTimestamp != timestamp)
+        window.location.href = window.location.href;
+  })
+}, 1000);
